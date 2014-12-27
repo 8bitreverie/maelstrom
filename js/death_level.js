807dadbebf86ed41555fe42d1811b5fb5feedfb7
Anthony Roe <roe.anthony@gmail.com>
2014-12-27 15:10:56 -0700

    Additional features
    Features
    -----------
    Finished 1, 2, 3 levels.
    Added shield after respawn.
    Added Delta time on movements.
    Added new music.
    Added story on scroller.
    Added end death scene.
    
    +Other bugfixes/polish

--- js/death_level.js
+++ js/death_level.js
@@ -11,24 +11,61 @@ var DEATH = {
     var deathScreenUI = new GameObject();
     deathScreenUI.behaviour = function () {
 
-      var score = this.level.engineRef.globals.score;
+
+      this.thisTime += Time.deltaTime();
+
+      if ((this.thisTime - this.lastTime) <= 0.1) {
+        return;
+      }
+
+      this.lastTime = this.thisTime;
+
+      var canvas = document.getElementsByTagName("canvas");
+      var color  = this.currentColor-=3;
+
+      //Shrink and vanish
+      if(this.currentColor<0) {
+        var size = this.size-=1;
+
+        var canvas = document.getElementsByTagName("canvas");
+        for (var i = 0; i < canvas.length; i++) {
+
+          if(size <= 0)
+            canvas[i].style.display = "none";
+
+          canvas[i].width  = size;
+          canvas[i].height = size;
+
+        }
+      }
+
+      //Set color
+      for (var i = 0; i < canvas.length; i++) {
+        canvas[i].style.backgroundColor = "rgb("+color+","+color+","+color+")";
+      }
 
       //TODO: Make this dynamic calculation
-      this.position.x = 45;
-      this.position.y = 125;
+      this.position.x = 55;
+      this.position.y = 250;
 
-      this.uiText = "You scored:" + score;
-      this.uiText += "\n Press Space To Play Again!";
+      this.uiText = "Hold Space To Continue To Live!";
 
       this.level.engineRef.globals.score = 0;
       this.level.engineRef.globals.lives = 3;
 
       if(Key.isDown(Key.SPACE)){
-        this.level.engineRef.loadLevel("main");
+        this.currentColor=0xff;
+        //this.level.engineRef.loadLevel("main");
+        //TODO: Adjust music volume/fx
       }
 
     };
-    deathScreenUI.isUI = true;
+
+    deathScreenUI.thisTime = 0.0;
+    deathScreenUI.lastTime = 0.1;
+    deathScreenUI.currentColor = 0xff;
+    deathScreenUI.size = 512;
+    deathScreenUI.isUI     = true;
     this.level.gameObjects = [deathScreenUI];
   },
 };
